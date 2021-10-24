eb7f1c4d04242bdc2ae2d89bcfa7da3388b6b75a
ScarlettSamurai <KAITDUFFEY@knights.ucf.edu>
2021-10-24 14:02:43 -0400

    Tier2Complete

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -114,7 +114,7 @@ public class PlayerScript : MonoBehaviour
         {
             if(Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0,4),ForceMode2D.Impulse);
+                rd2d.AddForce(new Vector2(0,5),ForceMode2D.Impulse);
             }
         }
     }
